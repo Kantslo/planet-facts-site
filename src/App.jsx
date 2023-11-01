940df86eaa43daf6fe0201e509d04ab9058b84e8
Giorgi Kantsliani <gqancliani@gmail.com>
2023-11-02 03:19:28 +0400

    commit

---
+++ src/App.jsx
@@ -0,0 +1,209 @@
+import data from "./data.json";
+import { Routes, Route, NavLink, Navigate } from "react-router-dom";
+import Planet from "./components/Planet";
+import { useState } from "react";
+import styled from "styled-components";
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
+  console.log(isOpen);
+
+  const Navigation = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 16px 24px;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+
+    @media (min-width: 768px) {
+      height: 160px;
+      flex-direction: column;
+    }
+
+    @media (min-width: 1440px) {
+      flex-direction: row;
+      height: 85px;
+    }
+  `;
+
+  const Hamburger = styled.svg`
+    background-image: url("/src/assets/icon-hamburger.svg");
+    background-repeat: no-repeat;
+    width: 24px;
+    height: 17px;
+    opacity: ${isOpen ? "0.5" : ""};
+
+    @media (min-width: 768px) {
+      display: none;
+    }
+  `;
+
+  const Header = styled.h1`
+    color: #fff;
+    font-size: 28px;
+    font-style: normal;
+    font-family: "Antonio", sans-serif;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: -1.05px;
+    text-transform: uppercase;
+
+    @media (min-width: 768px) {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  `;
+
+  const NavLinks = styled.ul`
+    display: ${isOpen ? "flex" : "none"};
+    position: ${isOpen ? "absolute" : ""};
+    background: ${isOpen ? "#070724" : ""};
+    width: ${isOpen ? "100%" : ""};
+    height: ${isOpen ? "540px" : ""};
+    top: 0;
+    left: 0;
+    flex-direction: column;
+    z-index: 10;
+    top: 68.5px;
+    width: 100%;
+    height: 598px;
+    padding-top: 24px;
+
+    @media (min-width: 768px) {
+      display: flex;
+      position: unset;
+      flex-direction: row;
+      height: unset;
+      border-bottom: none;
+      background: unset;
+    }
+
+    @media (min-width: 1440px) {
+      width: 50vw;
+      padding: 0;
+    }
+  `;
+
+  const PlanetList = styled.li`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    list-style: none;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+    padding: 20px 0;
+    width: 90%;
+    margin: 0 auto;
+    text-decoration: none;
+    height: 82px;
+
+    &:last-child {
+      border: none;
+    }
+
+    @media (min-width: 768px) {
+      border: none;
+      justify-content: space-evenly;
+    }
+
+    @media (min-width: 1440px) {
+      &:hover {
+        border-top: 4px solid ${(props) => props.planetColor};
+        a {
+          opacity: 1;
+        }
+      }
+    }
+  `;
+
+  const StyledNavLink = styled(NavLink)`
+    text-decoration: none;
+    color: #fff;
+    opacity: 0.75;
+    text-align: center;
+    font-family: "Spartan";
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 25px;
+    letter-spacing: 1.364px;
+    text-transform: uppercase;
+
+    @media (min-width: 768px) {
+      font-size: 11px;
+      letter-spacing: 1px;
+    }
+  `;
+
+  const ListIcon = styled.span`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+
+    @media (min-width: 768px) {
+      display: none;
+    }
+  `;
+
+  const NavLeft = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 25px;
+  `;
+
+  const ArrowIcon = styled.svg`
+    @media (min-width: 768px) {
+      display: none;
+    }
+  `;
+
+  return (
+    <>
+      <header>
+        <Navigation>
+          <Header>The Planets</Header>
+          <Hamburger onClick={toggleNav} />
+          <NavLinks>
+            {data.map((planet) => (
+              <PlanetList planetColor={planet.color}>
+                <NavLeft>
+                  <ListIcon
+                    style={{ background: `${planet.color}` }}></ListIcon>
+                  <StyledNavLink
+                    key={planet.name}
+                    to={`planets/${planet.name.toLowerCase()}`}
+                    onClick={() => {
+                      toggleNav();
+                    }}>
+                    {planet.name}
+                  </StyledNavLink>
+                </NavLeft>
+                <ArrowIcon
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="6"
+                  height="8">
+                  <path
+                    fill="none"
+                    stroke="#FFF"
+                    opacity=".4"
+                    d="M1 0l4 4-4 4"
+                  />
+                </ArrowIcon>
+              </PlanetList>
+            ))}
+          </NavLinks>
+        </Navigation>
+      </header>
+      <Routes>
+        <Route path="/" element={<Navigate to="/planets/mercury" />} />
+        <Route path="planets/:planetName" element={<Planet />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Planet.jsx
@@ -0,0 +1,440 @@
+import { useParams } from "react-router-dom";
+import data from "../data.json";
+import { useState } from "react";
+import styled from "styled-components";
+
+export default function Planet() {
+  const { planetName } = useParams();
+  const planet = data.find(
+    (planet) => planet.name.toLowerCase() === planetName.toLowerCase()
+  );
+
+  const [content, setContent] = useState(data[0].overview.content);
+
+  const [viewType, setViewType] = useState("overview");
+
+  console.log(planetName);
+
+  const ImageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 304px;
+
+    @media (min-width: 768px) {
+      height: 460px;
+      grid-area: 1 / 1 / 2 / 3;
+    }
+
+    @media (min-width: 1440px) {
+      height: 754px;
+      grid-area: 1 / 1 / 3 / 2;
+    }
+  `;
+
+  const PlanetImage = styled.img`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transform: scale(0.383);
+
+    @media (min-width: 768px) {
+      transform: scale(0.634015);
+    }
+
+    @media (min-width: 1440px) {
+      transform: scale(1);
+    }
+  `;
+
+  const PlanetSurface = styled.img`
+    position: absolute;
+    top: 20%;
+    transform: scale(0.2);
+
+    @media (min-width: 768px) {
+      transform: scale(0.35);
+      top: 45%;
+    }
+
+    @media (min-width: 1440px) {
+      transform: scale(0.6);
+      top: 55%;
+    }
+  `;
+
+  const PlanetInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    padding: 0 28px;
+
+    @media (min-width: 768px) {
+      width: 339px;
+      align-items: start;
+      grid-area: 2 / 1 / 3 / 2;
+      margin: 0 auto;
+    }
+
+    @media (min-width: 1440px) {
+      grid-area: 1 / 2 / 2 / 3;
+      padding: 0;
+    }
+  `;
+
+  const PlanetName = styled.h2`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    font-family: "Antonio";
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-transform: uppercase;
+    margin-bottom: 16px;
+
+    @media (min-width: 768px) {
+      font-size: 48px;
+      margin-bottom: 24px;
+    }
+
+    @media (min-width: 1440px) {
+      font-size: 80px;
+      margin-top: 126px;
+    }
+  `;
+
+  const Content = styled.p`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    text-align: center;
+    font-family: "Spartan";
+    font-size: 11px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 22px;
+    height: 132px;
+
+    @media (min-width: 768px) {
+      text-align: left;
+      height: 110px;
+    }
+
+    @media (min-width: 1440px) {
+      font-size: 14px;
+      line-height: 25px;
+    }
+  `;
+
+  const Source = styled.p`
+    color: #fff;
+    font-family: "Spartan";
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    opacity: 0.5;
+    margin-bottom: 28px;
+
+    @media (min-width: 768px) {
+      margin: 32px 0 41px;
+    }
+
+    @media (min-width: 1440px) {
+      font-size: 14px;
+      margin-bottom: 24px;
+    }
+  `;
+
+  const SourceLink = styled.a`
+    color: #fff;
+    font-family: "Spartan";
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 25px;
+  `;
+
+  const Properties = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-bottom: 47px;
+
+    @media (min-width: 768px) {
+      justify-content: space-between;
+      margin: 0 auto;
+      flex-direction: row;
+      grid-area: 3 / 1 / 4 / 3;
+    }
+
+    @media (min-width: 1440px) {
+      grid-area: 3 / 1 / 4 / 3;
+      gap: 30px;
+    }
+  `;
+
+  const PropertieDivs = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border: 1px solid rgba(255, 255, 255, 0.2);
+    width: 327px;
+    height: 48px;
+    flex-shrink: 0;
+    padding: 0 24px;
+
+    @media (min-width: 768px) {
+      flex-direction: column;
+      align-items: start;
+      width: 164px;
+      height: 88px;
+      padding: 16px 15px;
+      margin-top: 41px;
+    }
+
+    @media (min-width: 1440px) {
+      width: 255px;
+      height: 128px;
+      justify-content: center;
+      padding: 20px 23px;
+    }
+  `;
+
+  const PropertieName = styled.p`
+    color: #fff;
+    font-family: "Spartan";
+    font-size: 8px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 16px;
+    letter-spacing: 0.727px;
+    text-transform: uppercase;
+    opacity: 0.5;
+
+    @media (min-width: 1440px) {
+      font-size: 11px;
+      letter-spacing: 1px;
+      line-height: 25px;
+    }
+  `;
+
+  const PropertieValues = styled.span`
+    color: #fff;
+    text-align: right;
+    font-family: "Antonio";
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: -0.75px;
+    text-transform: uppercase;
+
+    @media (min-width: 768px) {
+      font-size: 24px;
+    }
+
+    @media (min-width: 1440px) {
+      font-size: 40px;
+      letter-spacing: -1.5px;
+    }
+  `;
+
+  const PlanetContent = styled.div`
+    @media (min-width: 768px) {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      grid-template-rows: repeat(3, 0.5fr);
+      grid-column-gap: 0px;
+      grid-row-gap: 0px;
+    }
+  `;
+
+  const ViewBtn = styled.button`
+    border: none;
+    outline: none;
+    background: transparent;
+    height: 50px;
+    border-bottom: ${(props) =>
+      props.active ? `4px solid ${planet.color}` : ""};
+
+    @media (min-width: 768px) {
+      width: 281px;
+      height: 40px;
+      flex-shrink: 0;
+      border: 1px solid rgba(255, 255, 255, 0.2);
+      background: ${(props) => (props.active ? planet.color : "")};
+      border: ${(props) => (props.active ? "none" : "")};
+    }
+
+    @media (min-width: 1440px) {
+      width: 350px;
+      height: 48px;
+      flex-shrink: 0;
+      &:hover {
+        background: ${(props) =>
+          props.active ? "" : "rgba(216, 216, 216, 0.2)"};
+        border: none;
+      }
+    }
+  `;
+
+  const ViewLists = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+
+    @media (min-width: 768px) {
+      justify-content: start;
+      flex-direction: column;
+      gap: 16px;
+      border: none;
+      grid-area: 2 / 2 / 3 / 3;
+    }
+
+    @media (min-width: 1440px) {
+      grid-area: 2 / 2 / 3 / 3;
+    }
+  `;
+
+  const ViewText = styled.p`
+    color: white;
+    font-family: "Spartan";
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: 1.929px;
+    text-transform: uppercase;
+    font-size: 9px;
+    cursor: pointer;
+
+    @media (min-width: 768px) {
+      display: flex;
+      align-items: center;
+      padding: 0 20px;
+    }
+  `;
+
+  const ViewListNumber = styled.span`
+    display: none;
+
+    @media (min-width: 768px) {
+      display: inline;
+      opacity: 0.5;
+      padding-right: 14px;
+      color: #fff;
+      font-family: "Spartan";
+      font-size: 9px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 25px;
+      letter-spacing: 1.929px;
+      text-transform: uppercase;
+    }
+  `;
+
+  return (
+    <>
+      <div className="planet">
+        <PlanetContent>
+          <ViewLists>
+            <ViewBtn
+              active={viewType === "overview"}
+              onClick={() => {
+                setViewType("overview");
+                setContent(planet.overview.content);
+              }}>
+              <ViewText>
+                <ViewListNumber>01</ViewListNumber>
+                Overview
+              </ViewText>
+            </ViewBtn>
+            <ViewBtn
+              active={viewType === "structure"}
+              onClick={() => {
+                setViewType("structure");
+                setContent(planet.structure.content);
+              }}>
+              <ViewText>
+                <ViewListNumber>02</ViewListNumber>
+                Structure
+              </ViewText>
+            </ViewBtn>
+            <ViewBtn
+              active={viewType === "surface"}
+              onClick={() => {
+                setViewType("surface");
+                setContent(planet.geology.content);
+              }}>
+              <ViewText>
+                <ViewListNumber>03</ViewListNumber>Surface
+              </ViewText>
+            </ViewBtn>
+          </ViewLists>
+
+          <ImageContainer>
+            <PlanetImage
+              src={
+                viewType === "structure"
+                  ? planet.images.internal
+                  : planet.images.planet
+              }
+              alt="image"
+            />
+            {viewType === "surface" && (
+              <PlanetSurface src={planet.images.geology} alt="image" />
+            )}
+          </ImageContainer>
+
+          <PlanetInfo>
+            <PlanetName>{planet.name}</PlanetName>
+            <Content>{content}</Content>
+            <Source>
+              Source <span>:</span>
+              <SourceLink href={planet.overview.source}>Wikipedia </SourceLink>
+              <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12">
+                <path
+                  fill="#FFF"
+                  d="M11.34.66C10.9.22 10.37 0 9.75 0h-7.5C1.63 0 1.1.22.66.66.22 1.1 0 1.63 0 2.25v7.5c0 .62.22 1.15.66 1.59.44.44.97.66 1.59.66h7.5c.62 0 1.15-.22 1.59-.66.44-.44.66-.97.66-1.59v-7.5c0-.62-.22-1.15-.66-1.59zM10 6.25a.467.467 0 01-.305.46.544.544 0 01-.195.04.465.465 0 01-.352-.149L8.023 5.476 3.852 9.648a.481.481 0 01-.352.149.48.48 0 01-.352-.149l-.796-.797a.48.48 0 01-.149-.351.48.48 0 01.149-.352l4.172-4.172-1.125-1.125c-.162-.15-.198-.333-.11-.546A.467.467 0 015.75 2H9.5c.135 0 .253.05.352.148A.48.48 0 0110 2.5v3.75z"
+                  opacity=".5"
+                />
+              </svg>
+            </Source>
+          </PlanetInfo>
+          <Properties>
+            <PropertieDivs>
+              <PropertieName>rotation time</PropertieName>
+              <PropertieValues>{planet.rotation}</PropertieValues>
+            </PropertieDivs>
+            <PropertieDivs>
+              <PropertieName>revolution time</PropertieName>
+              <PropertieValues>{planet.revolution}</PropertieValues>
+            </PropertieDivs>
+            <PropertieDivs>
+              <PropertieName>radius</PropertieName>
+              <PropertieValues>{planet.radius}</PropertieValues>
+            </PropertieDivs>
+            <PropertieDivs>
+              <PropertieName>average temp.</PropertieName>
+              <PropertieValues>{planet.temperature}</PropertieValues>
+            </PropertieDivs>
+          </Properties>
+        </PlanetContent>
+      </div>
+    </>
+  );
+}
